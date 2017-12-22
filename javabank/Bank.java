46ca3673e4c8ffeb078594493542cbb145a01ebc
Sam Ellenrieder <sam.ell@web.de>
2017-12-22 17:24:23 +0000

    still initializing.....

--- javabank/Bank.java
+++ javabank/Bank.java
@@ -1,22 +1,15 @@
 public class Bank {
- 
-    public static String name;
-    public static String balance = 0;
-
+    
+        public static String name;
+        public static String balance = 0;
 
     public static void main(String[] args){
 
 
-  	  public String ObjectIUse(String, name){
-    	    this.name = name;
+    }
+
+    public String ObjectIUse(String, name){
+          this.name = name;
       }
-
-   //  public String getName(){
-   //    return name;
-   //  }
-
-  	  System.out.println("Hello");
-
-  }
 }
 

d0debb30020d30620b7b71f09da7de0c87812be7
Sam Ellenrieder <sam.ell@web.de>
2017-12-22 17:00:35 +0000

    initialize method

---
+++ javabank/Bank.java
@@ -0,0 +1,22 @@
+public class Bank {
+ 
+    public static String name;
+    public static String balance = 0;
+
+
+    public static void main(String[] args){
+
+
+  	  public String ObjectIUse(String, name){
+    	    this.name = name;
+      }
+
+   //  public String getName(){
+   //    return name;
+   //  }
+
+  	  System.out.println("Hello");
+
+  }
+}
+

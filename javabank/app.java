e9a65764c53e996e555852dd53bb70a58fbfdecb
Sam Ellenrieder <sam.ell@web.de>
2017-12-22 15:42:23 +0000

    Java bank tech test start

---
+++ javabank/app.java
@@ -0,0 +1,21 @@
+class Bank {
+ 
+    static String name;
+    // Private int balance;
+
+
+  public static void main(String[] args){
+
+  	// String ObjectIUse(String, name){
+   //  	this.name = name;
+   //  }
+
+   //  public String getName(){
+   //    return name;
+   //  }
+
+  	System.out.println("Hello");
+
+  }
+}
+
